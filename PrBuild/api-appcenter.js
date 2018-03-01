f0cb9296e4df1bf018a6b4cad8281b7af1ca4902
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-03-01 14:49:10 +0300

    Added error handling to appcenter api.

--- PrBuild/api-appcenter.js
+++ PrBuild/api-appcenter.js
@@ -1,12 +1,23 @@
 const request = require('request-promise');
 
 module.exports = {
-    getBuildConfiguration: function(branch, token, owner, app) {
+    getBuildConfiguration: function (branch, token, owner, app) {
         const endpoint = `/branches/${branch}/config`;
         var options = BuildUrl(endpoint, token, owner, app);
-        return request(options);
+        var f = typeof (request(options));
+        return request(options).then(() => { }, (error) => {
+            if (error.statusCode === 404) {
+                error.message = error.message + "Error: 404 Not Found. Please check you have pasted valid appcenter owner, owner type and app name in config.json.";
+                return Promise.reject(error)
+            } else if (error.statusCode == 401) {
+                error.message = error.message + "Error: 401 Unauthorized. Could not login to appCenter. Please check you have pasted valid appcenter token in local.settings.json.";
+                return Promise.reject(error);
+            } else {
+                return Promise.reject(error);
+            }
+        });
     },
-    createPrBuildConfiguration: function(config, branch, token, owner, app) {
+    createPrBuildConfiguration: function (config, branch, token, owner, app) {
         // Disable distribute on build and change name over to new branch
         config.toolsets.distribution = {};
         config.branch.name = branch;
@@ -16,14 +27,14 @@ module.exports = {
         Object.assign(options, { method: 'POST', body: JSON.stringify(config) });
         return request(options);
     },
-    startPrBuild: function(branch, sha, token, owner, app) {
+    startPrBuild: function (branch, sha, token, owner, app) {
         const payload = { sourceVersion: sha };
 
         const options = BuildUrl(`/branches/${branch}/builds`, token, owner, app);
         Object.assign(options, { method: 'POST', body: JSON.stringify(payload) });
         return request(options);
     },
-    deletePrBuildConfiguration: function(branch, token, owner, app) {
+    deletePrBuildConfiguration: function (branch, token, owner, app) {
         const options = BuildUrl(`/branches/${branch}/config`, token, owner, app);
         Object.assign(options, { method: 'DELETE' });
         return request(options);
