eb3b26ce50b650d32b585863cc3a0a800c1d423c
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-03-01 14:49:33 +0300

    Added error handling to github api.

--- PrBuild/api-github.js
+++ PrBuild/api-github.js
@@ -1,7 +1,7 @@
 const request = require('request-promise');
 
 module.exports = {
-    reportGithubStatus: function(repo_path, branch, sha, token, owner, owner_type, app, buildNumber) {
+    reportGithubStatus: function (repo_path, branch, sha, token, owner, owner_type, app, buildNumber) {
         const options = {
             headers: { 'Accept': 'application/json', 'User-Agent': 'appcenter-ci', 'Content-Type': 'application/json', 'Authorization': `token ${token}` },
             url: `https://api.github.com/repos/${repo_path}/statuses/${sha}`
@@ -9,13 +9,20 @@ module.exports = {
 
         var report = {
             state: 'pending',
-            target_url: `https://appcenter.ms/${owner_type}/${owner}/apps/${app}/build/branches/${branch}/builds/${buildNumber}`,
+            target_url: `https://appcenter.ms/${owner_type}/${owner}/apps/${app}/build/branches/${branch}/build/${buildNumber}`,
             description: 'Running build in App Center...',
             context: `appcenter-ci/${app}`
         };
 
         Object.assign(options, { method: 'POST', body: JSON.stringify(report) });
 
-        return request(options);
+        return request(options).then(() => { },
+            (error) => {
+                if (error.statusCode == 404 || error.statusCode == 401) {
+                    return Promise.reject("Error sending status to github. Please check you have pasted valid github token, repo_owner and repo_name in local.settings.json and config.json.")
+                } else {
+                    return Promise.reject(error);
+                }
+            });
     }
 };
