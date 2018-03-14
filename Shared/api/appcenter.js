1da90458c4e9bae2b5a9d44e632386510e82d7d9
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-03-14 22:06:47 +0300

    Added comments for appcenter API

--- Shared/api/appcenter.js
+++ Shared/api/appcenter.js
@@ -1,16 +1,19 @@
 const request = require('request-promise');
 
 module.exports = {
+    //Gets the specified AppCenter app by its name and owner.
     getApp: function(token, owner, app) {
         const options = BuildUrl('', token, owner, app);
         Object.assign(options, { method: 'GET' });
         return request(options);
     },
+    //Gets the specified app's repository configuration (info about the linked GitHub repo).
     getConfig: function(token, owner, app) {
         const options = BuildUrl('/repo_config', token, owner, app);
         Object.assign(options, { method: 'GET' });
         return request(options);
     },
+    //Gets all apps in AppCenter by owner's token.
     getAllApps: function(token) {
         const options = {
             headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', 'X-API-Token': token },
@@ -19,11 +22,13 @@ module.exports = {
         Object.assign(options, { method: 'GET' });
         return request(options);
     },
+    //Gets the build configuration of the specified branch.
     getBuildConfiguration: function(branch, token, owner, app) {
         const endpoint = `/branches/${branch}/config`;
         const options = BuildUrl(endpoint, token, owner, app);
         return request(options);
     },
+    //Creates a build configuration on the specified branch.
     createPrCheckConfiguration: function(config, branch, token, owner, app) {
         // Force simulator build, disable distribute on build, signing 
         // and change name over to new branch
@@ -39,6 +44,7 @@ module.exports = {
         Object.assign(options, { method: 'POST', body: JSON.stringify(config) });
         return request(options);
     },
+    //Starts AppCenter branch build.
     startPrCheck: function(branch, sha, token, owner, app) {
         const payload = { sourceVersion: sha };
 
@@ -46,21 +52,25 @@ module.exports = {
         Object.assign(options, { method: 'POST', body: JSON.stringify(payload) });
         return request(options);
     },
+    //Deletes AppCenter branch build configuration.
     deletePrCheckConfiguration: function(branch, token, owner, app) {
         const options = BuildUrl(`/branches/${branch}/config`, token, owner, app);
         Object.assign(options, { method: 'DELETE' });
         return request(options);
     },
+    //Gets the specified branch builds.
     getBuilds: function(branch, token, owner, app) {
         const options = BuildUrl(`/branches/${branch}/builds`, token, owner, app);
         Object.assign(options, { method: 'GET' });
         return request(options);
     },
+    //Gets the specified build by build id.
     getBuild: function(build_id, token, owner, app) {
         const options = BuildUrl(`/builds/${build_id}`, token, owner, app);
         Object.assign(options, { method: 'GET' });
         return request(options);
     },
+    //Stops (cancels) the specified build by build id.
     stopBuild: function(build_id, token, owner, app) {
         const payload = { status: 'cancelling' };
         const options = BuildUrl(`/builds/${build_id}`, token, owner, app);
